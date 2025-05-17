3e9607c60bcee86c2262353077dadeeb56904b16
Austin Brown <austinj.brown2005@gmail.com>
2025-05-17 14:20:56 -0500

    Finished Item class

--- RoomAdventure_JavaProject/RoomAdventure.java
+++ RoomAdventure_JavaProject/RoomAdventure.java
@@ -28,21 +28,21 @@ public class RoomAdventure{ // Main class containing game logic
     }
 
     private static void handlelook(String noun) { // Handles looking at items
-        String[] items = currentRoom.getItems(); // Get the items in the room
-        String[] itemDescriptions = currentRoom.getItemDescriptions(); // Get the item descriptions
+        Item[] items = currentRoom.getItems(); // Get the items in the room
         status = "I don't see that item."; // Default if the item is not found
-        for (int i = 0; i < items.length; i++) { // Loop through the items
-            if (noun.equals(items[i])) { // Check if the item is valid
-                status = itemDescriptions[i]; // Update status with description
+        for (Item item : items) { // Loop through the items
+            if (noun.equals(item.getItemName())) { // Check if the item is valid
+                status = item.getItemDescription();// Update status with description
+                break; 
             }
         }
     }
 
     private static void handleTake(String noun) { // Handles taking items
-        String[] grabbables = currentRoom.getGrabbables(); // Items that can be taken
+        Item[] items = currentRoom.getItems(); // Items that can be taken
         status = "I cant take that item."; // Default if the item is not grabbable
-        for (String item: grabbables){ // Loop through the grabbables
-            if (noun.equals(item)) { // Check if the item is valid
+        for (Item item : items){ // Loop through the grabbables
+            if (noun.equals(item.getItemName()) && item.getIsGrabbable()) { // Check if the item is valid
                 for (int j = 0; j < inventory.length; j++) { // Loop through the inventory
                     if (inventory[j] == null) { // Check for empty slot
                         inventory[j] = noun; // Add item to inventory
@@ -86,14 +86,14 @@ public class RoomAdventure{ // Main class containing game logic
         // Room 2 Items
         Item fireplace = new Item(
             "fireplace",
-            "This is a fireplace", 
+            "Its on fire", 
             false, 
             false, 
             false
         );
         Item rug = new Item(
             "rug", 
-            "This is a rug", 
+            "theres a lump of coal on the rug", 
             false, 
             false, 
             false
@@ -108,7 +108,7 @@ public class RoomAdventure{ // Main class containing game logic
         // Room 3 Items
         Item bookshelves = new Item(
             "bookshelves", 
-            "There is a book on one of the shelves", 
+            "There is nothing on it. Go figure.", 
             false, 
             false, 
             true
@@ -122,7 +122,7 @@ public class RoomAdventure{ // Main class containing game logic
         );
         Item desk2 = new Item(
             "desk", 
-            "This is a desk", 
+            "This is a desk. There is a book on it", 
             false, 
             false, 
             false
@@ -135,74 +135,60 @@ public class RoomAdventure{ // Main class containing game logic
             true
         );
         // Room 4 Items
+        Item brew_rig = new Item(
+            "brew rig",
+             "Gourd is brewing some sort of oatmeal stout on the brewrig. A 6-pack is resting beside it.", 
+             false, 
+             false, 
+             false
+             );
+        Item chest = new Item(
+            "chest",
+            "It is made of a dark oak. It is locked. Maybe USE a key to open it.",
+            false,
+            false,
+            false
+        );
+        Item six_pack = new Item(
+            "6 pack",
+            "This is a 6 pack of beer",
+            false,
+            true,
+            true
+        );
+
 
         // ################################# Room 1 #################################
 
         String[] room1ExitDirections = {"east", "south"}; // Exit directions for room 1
         Room[] room1ExitDestinations = {room2, room4}; // Exit destinations for room 1
-        String[] room1Items = {chair.getItemName(), desk.getItemName()}; // Items in room 1
-        String[] room1ItemDescriptions = {
-            chair.getItemDescription(), 
-            desk.getItemDescription()
-        }; // Item descriptions for room 1
-
-        String[] room1Grabbables = {"key"}; // items in room 1 you can take
         room1.setExitDirections(room1ExitDirections); // Set exit directions for room 1
         room1.setExitDestinations(room1ExitDestinations); // Set exit destinations for room 1
-        room1.setItems(room1Items); // Set items for room 1
-        room1.setItemDescriptions(room1ItemDescriptions); // Set item descriptions for room 1
-        room1.setGrabbables(room1Grabbables); // Set grabbables for room 1
+        room1.setItems(new Item[]{chair, desk, key}); // Set items for room 1
 
         // ################################# Room 2 #################################
 
         String[] room2ExitDirections = {"west", "south"}; // Exit directions for room 2
         Room[] room2ExitDestinations = {room1, room3}; // Exit destinations for room 2
-        String[] room2Items = {"fireplace", "rug"}; // Items in room 2
-        String[] room2ItemDescriptions = {
-            "Its on fire",
-            "theres a lump of coal on the rug"
-        }; // Item descriptions for room 2
-        String[] room2Grabbables = {"coal"}; // items in room 2 you can take
         room2.setExitDirections(room2ExitDirections); // Set exit directions for room 2
         room2.setExitDestinations(room2ExitDestinations); // Set exit destinations for room 2
-        room2.setItems(room2Items); // Set items for room 2
-        room2.setItemDescriptions(room2ItemDescriptions); // Set item descriptions for room 2
-        room2.setGrabbables(room2Grabbables); // Set grabbables for room 2
+        room2.setItems(new Item[]{fireplace, rug, coal}); // Set items for room 2
 
         // ################################# Room 3 #################################
 
         String[] room3ExitDirections = {"north", "west"}; // Exit directions for room 3
         Room[] room3ExitDestinations = {room2, room4}; // Exit destinations for room 3
-        String[] room3Items = {"bookshelves", "statue", "desk"}; // Items in room 3
-        String[] room3ItemDescriptions = {
-            "They are empty. Go Figure.", 
-            "There is nothing special about it.",
-            "The statue is resting on it. So is a book."
-        }; // Item descriptions for room 3
-
-        String[] room3Grabbables = {"book"}; // items in room 3 you can take
         room3.setExitDirections(room3ExitDirections); // Set exit directions for room 3
         room3.setExitDestinations(room3ExitDestinations); // Set exit destinations for room 3
-        room3.setItems(room3Items); // Set items for room 3
-        room3.setItemDescriptions(room3ItemDescriptions); // Set item descriptions for room 3
-        room3.setGrabbables(room3Grabbables); // Set grabbables for room 3
+        room3.setItems(new Item[]{bookshelves, statue, desk2, book}); // Set items for room 3
 
          // ################################# Room 4 #################################
 
         String[] room4ExitDirections = {"east", "north"}; // Exit directions for room 4
         Room[] room4ExitDestinations = {room3, room1}; // Exit destinations for room 4
-        String[] room4Items = {"brew_rig", "chest"}; // Items in room 4
-        String[] room4ItemDescriptions = {
-            "Gourd is brewing some sort of oatmeal stout on the brewrig. A 6-pack is resting beside it.", 
-            "It is made of a dark oak. It is locked. Maybe USE a key to open it."
-        }; // Item descriptions for room 4
-
-        String[] room4Grabbables = {"6-pack"}; // items in room 4 you can take
         room4.setExitDirections(room4ExitDirections); // Set exit directions for room 4
         room4.setExitDestinations(room4ExitDestinations); // Set exit destinations for room 4
-        room4.setItems(room4Items); // Set items for room 4
-        room4.setItemDescriptions(room4ItemDescriptions); // Set item descriptions for room 4
-        room4.setGrabbables(room4Grabbables); // Set grabbables for room 4
+        room4.setItems(new Item[]{brew_rig, chest, six_pack}); // Set items for room 4
 
         // Set the current room to room 1
         currentRoom = room1; // Set the current room to room 1
@@ -270,9 +256,8 @@ class Room{ // Represents a game room
     private String name; // Room name
     private String[] exitDirections; // Directions you can go to exit  
     private Room[] exitDestinations; // Rooms reached by each direciton 
-    private String[] items; // Items visible in each room 
-    private String[] itemDescriptions; // Descriptions of items
-    private String[] grabbables; // Items you can take
+    private Item[] items; // Items visible in each room 
+
 
 
     public Room(String name) { // Constructor for room
@@ -296,39 +281,19 @@ class Room{ // Represents a game room
         return exitDirections; // Return the exit directions
     }
 
-    public void setItems(String[] items){ // Setter for items
+    public void setItems(Item[] items){ // Setter for items
         this.items = items; // Set the items
     }
 
-    public String[] getItems(){ // Getter for items
+    public Item[] getItems(){ // Getter for items
         return items; // Return the items
     }
 
-    public void setItemDescriptions(String[] itemDescriptions){ // Setter for item descriptions
-        this.itemDescriptions = itemDescriptions; // Set the item descriptions
-    }
-
-    public String[] getItemDescriptions(){ // Getter for item descriptions
-        return itemDescriptions; // Return the item descriptions
-    }
-
-    public void setGrabbables(String[] grabbables){ // Setter for grabbables
-        this.grabbables = grabbables; // Set the grabbables
-    }
-
-    public String[] getGrabbables(){ // Getter for grabbables
-        return grabbables; // Return the grabbables
-    }
-
-// NOTE TO SELF: public void ...(type, varible){ // Setters
-// NOTE TO SELF: public type get...(type, varible){ // Getters
-
-    // @Override 
     public String toString(){ // Custom print for the room
         String result = "\nLocation: " + name; //Show room name
         result += "\nYou See: "; // Show what you see
-        for (String item : items){ //Loop items
-            result += item + " "; // apend each item
+        for (Item item : items){ //Loop items
+            result += item.getItemName() + " "; // apend each item
             }
         result += "\nExits: "; // List exits
         for (String direction : exitDirections) { // Loop exits
